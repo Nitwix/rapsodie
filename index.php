71637d2a58e521b637ec76def7e421aef509124d
Niels Lachat <nielsnfsmw@gmail.com>
2017-08-04 12:12:53 +0200

    commit avant diner

--- index.php
+++ index.php
@@ -7,19 +7,19 @@
 			<div class="row">
 				<div class="col-sm-2"></div> <!-- so it's aligned in the middle -->
 				<div class="col-sm-8">
-					<h1 class="text-center text"></h1>
-					<h3 class="text-center text">ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</h3>
+					<h1 class="text-center styled_text super_title">LES CHAISES <small>Steve Muriset</small></h1>
 				</div>
 				<div class="col-sm-2"></div>
 			</div>
 			<div class="row">
-				<!--<div class="col-sm-2"></div>-->
-				<div class="col-sm-6 col-xs-12">
-					<img class="img-responsive" src="">
+				<div class="col-sm-2"></div> <!-- so it's aligned in the middle -->
+				<div class="col-sm-8">
+					<h2 class="styled_text">Représentations</h1>
+
+					<h3 class="text-center styled_text">Lussan, France</h3>
+					<h4 class="styled_text">8 et 9 août 2017, La Filature – Festival « Entre Ciel et Garigue », www.festival-lussan.fr</h4>
 				</div>
-				<div class="col-sm-6 col-xs-12">
-					<img class="img-responsive" src="">
-				</div>
+				<div class="col-sm-2"></div>
 			</div>
 		</div>
 		<?php include "includes/footer.php" ?>
