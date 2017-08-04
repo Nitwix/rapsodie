15dc6edfa9479c563636019f2ef865c239e9f4e1
Niels Lachat <nielsnfsmw@gmail.com>
2017-08-04 15:46:01 +0200

    index nearly finished

--- index.php
+++ index.php
@@ -1,35 +1,71 @@
 <!DOCTYPE html>
 <html>
-	<?php include "includes/head.php" ?>
-	<body onclick="collapseNavbar()">
-		<?php include "includes/navbar.php";?>
-		<div class="container" > <!-- collapses the navbar when the 'content' is clicked-->
-			<div class="row">
-				<div class="col-sm-2"></div> <!-- so it's aligned in the middle -->
-				<div class="col-sm-8">
-					<h1><span class="styled_text">LES CHAISES </span><small>Steve Muriset</small></h1>
-				</div>
-				<div class="col-sm-2"></div>
-			</div>
-			<div class="row">
-				<div class="col-sm-2 col-lg-1"></div> <!-- so it's aligned in the middle -->
-				<div class="col-sm-8 col-lg-10">
-					<h2 class="styled_text">Représentations</h1>
+<?php include "includes/head.php" ?>
+<body onclick="collapseNavbar()">
+	<?php include "includes/navbar.php";?>
+	<div class="container-fluid" > <!-- collapses the navbar when the 'content' is clicked-->
+		<div class="row">
+			<div class="col-sm-2"></div> <!-- so it's aligned in the middle -->
+			<div class="col-sm-8">
+				<h1><span class="styled_text">LES CHAISES </span><small>Steve Muriset</small></h1>
 
-					<h3 class="text-center styled_text">Lussan, France</h3>
-					<h4><span class="styled_text">8 et 9 août 2017, La Filature – Festival « Entre Ciel et Garigue »,</span>
+			</div>
+			<div class="col-sm-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-8 col-lg-8 text-center">
+				<h2 class="styled_text">Représentations</h2>
+				<h3 class="styled_text">Lussan, France</h3>
+				<h4><span class="styled_text">8 et 9 août 2017, La Filature – Festival « Entre Ciel et Garigue »,</span>
+					<br>
 					<a href="http://festival-lussan.fr" id="www.festival-lussan.fr">www.festival-lussan.fr</a></h4>
 
-					<h3 class="text-center styled_text">Neuchâtel</h3>
-					<h4><span class="styled_text">1er, 3, 8 et 10 septembre 2017, Théâtre du Passage, Le Strapontin : 032 717 79 07 </span>
-					<a href="mailto:billetterie@theatredupassage.ch" id="billetterie@theatredupassage.ch">billetterie@theatredupassage.ch</a></h4>
+					<br>
 
-					<h3 class="text-center styled_text">Couvet</h3>
-					<h4>18 et 19 novembre 2017, Chapelle aux Concerts, pas de réservation</h4>
+					<h3 class="styled_text">Neuchâtel</h3>
+					<h4 class=""><span class="styled_text">1er, 3, 8 et 10 septembre 2017, Théâtre du Passage, Le Strapontin : </span>
+						<br>
+						<a href="qrcode.php" id="+41 32 717 79 07">+41 32 717 79 07</a>
+						<br>
+						<a href="mailto:billetterie@theatredupassage.ch" id="billetterie@theatredupassage.ch">billetterie@theatredupassage.ch</a></h4>
+
+						<br>
+
+						<h3 class="styled_text">Couvet</h3>
+						<br>
+						<h4 class="styled_text">18 et 19 novembre 2017, Chapelle aux Concerts, pas de réservation</h4>
+
+						<br>
+
+						<h2><span class="styled_text">Distribution</span></h2>
+						<h3><span class="styled_text">La Vieille : </span><strong>Alice Grandjean</strong></h3>
+						<h3><span class="styled_text">Le Vieux : </span><strong>Léonard Schneider</strong></h3>
+						<h3><span class="styled_text">L'orateur : </span><strong>Vincent Steiner</strong></h3>
+
+						<br>
+
+						<h3><span class="styled_text">Piano : </span><strong>Carolyn Woods</strong></h3>
+						<h3><span class="styled_text">Accordéon : </span><strong>Cédric Liardet</strong></h3>
+
+						<br>
+
+						<h3><span class="styled_text">Mise en scène : </span><strong>Mathieu Alexandre</strong></h3>
+						<h3><span class="styled_text">Direction artistique et composition : </span><strong>Steve Muriset</strong></h3>
+						<h3><span class="styled_text">Décors : </span><strong>Frédéric Erb</strong></h3>
+						<h3><span class="styled_text">Costumes : </span><strong>Lucienne Amort-Muriset</strong></h3>
+						<h3><span class="styled_text">Eclairage, Maquillage : </span><strong>Faustine De Montmollin Brenier</strong></h3>
+						<h3><span class="styled_text">Chargé de Projet : </span><strong>Hugo Beretta</strong></h3>
+
+						<br>
+
+					</div>
+
+					<div class="col-sm-4 col-lg-4">
+						<img class="img-responsive" src="images/flyer.png">
+					</div>
+
 				</div>
-				<div class="col-sm-2 col-lg-1"></div>
 			</div>
-		</div>
-		<?php include "includes/footer.php" ?>
-	</body>
-</html>
+			<?php include "includes/footer.php" ?>
+		</body>
+		</html>
---
+++ qrcode.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<?php include "includes/head.php" ?>
+<body onclick="collapseNavbar()">
+	<?php include "includes/navbar.php";?>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h3 class="text-center">Scannez ce QR code avec votre téléphone pour appeler la billeterie ou cliquez 
+					<a href="tel:+41327177907" id="ici">ici</a> si vous êtes sur un téléphone</h3>
+				</div>
+
+				<div class="col-xs-3 col-md-4"></div>
+
+				<div class="col-xs-6 col-md-4">
+					<br>
+					<img src="images/tel_num_qr.png" class="img-responsive center-block">
+				</div>
+
+				<div class="col-xs-3 col-md-4"></div>
+
+			</div>	
+		</div>
+		<?php include "includes/footer.php" ?>
+	</body>
+	</html>
