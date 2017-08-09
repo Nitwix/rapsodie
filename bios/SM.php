af5ee995f55328be2415188dd3d5d763f5cff2c3
Niels Lachat <nielsnfsmw@gmail.com>
2017-08-09 12:25:20 +0200

    bios en cours

---
+++ bios/SM.php
@@ -0,0 +1,32 @@
+<?php
+$content = "<div id='AG' class='tab-pane fade in active'>
+
+		<div class='row'>
+		
+			<div class='col-xs-12 col-md-9'>
+			
+				<h2>Alice Grandjean, soprano</h2>
+
+				<h4>
+				Née en 1993, Alice Grandjean est passionnée de musique dès son plus jeune âge. En 2006, elle commence le chant lyrique auprès d’Yves Senn et rejoint la compagnie de « L’avant-scène opéra »  participant ainsi à de nombreux spectacles. Elle est reçue au Conservatoire royal de Bruxelles, section chant, dans la classe de Marcel Vanaud et de Michèle Massina. Elle y obtient son Bachelor en juillet 2016. Elle a également travaillé avec Stuart Patterson et Ana-Camelia Stefanescu, et a suivi des masterclass auprès de Monika Lukacs et de Claudia Rüggeberg. Aujourd’hui, elle continue à se former auprès d’Yves Senn.
+				</h4>
+
+				<h4>
+				Depuis 2014, Alice Grandjean chante comme soliste en Suisse, en Belgique et en France. Elle a interprété entre autre les rôles de Papagena (Die Zauberflöte, Mozart), de Barbarina (Le Nozze di Figaro, Mozart), de Lucy (The Telephone, Menotti) ou encore de Belinda (Dido and Aeneas, Purcell), ainsi que de nombreux rôles d’opérette, tels que Zoé (La Cocarde de Mimi Pinson, Goublier fils), Louise (Les Mousquetaires au couvent, Varney) ou encore Cupidon (Orphée aux Enfers, Offenbach).
+				</h4>
+
+				<h4>
+				En 2015, elle cofonde la Compagnie d’opéra « Entre le Ziste et le Zeste », avec laquelle elle a déjà monté plusieurs spectacles. Par ailleurs, elle collabore également régulièrement avec différentes formations, telles que « Les Divertissements royaux », « L’avant-scène opéra », ainsi que l’ensemble baroque « Hortus Amoris ».
+				</h4>
+				
+			</div>
+			
+			<div class='col-xs-6 col-md-3'>
+				<img class='img-responsive center-block' src='images/bios/AG.jpg'>
+			</div>
+		
+		</div>
+		
+		</div>";
+echo $content;
+?>
