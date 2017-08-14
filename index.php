b7b2387c92748e86810b919aa8a5a5373fabd0d9
Niels Lachat <nielsnfsmw@gmail.com>
2017-08-14 09:16:35 +0200

    mise en forme du code améliorée

--- index.php
+++ index.php
@@ -14,56 +14,61 @@
 		<div class="row">
 			<div class="col-sm-8 col-lg-8 text-center">
 				<h2 class="styled_text">Représentations</h2>
+
 				<h3 class="styled_text">Lussan, France</h3>
-				<h4><span class="styled_text">8 et 9 août 2017, La Filature – Festival « Entre Ciel et Garigue »,</span>
+				<h4>
+					<span class="styled_text">8 et 9 août 2017, La Filature – Festival « Entre Ciel et Garigue »,</span>
 					<br>
-					<a href="http://festival-lussan.fr" id="www.festival-lussan.fr">www.festival-lussan.fr</a></h4>
+					<a href="http://festival-lussan.fr" id="www.festival-lussan.fr">www.festival-lussan.fr</a>
+				</h4>
 
+				<br>
+
+				<h3 class="styled_text">Neuchâtel</h3>
+				<h4>
+					<span class="styled_text">1er, 3, 8 et 10 septembre 2017, Théâtre du Passage, Le Strapontin : </span>
 					<br>
+					<a href="qrcode.php" id="+41 32 717 79 07">+41 32 717 79 07</a>
+					<br>
+					<a href="mailto:billetterie@theatredupassage.ch" id="billetterie@theatredupassage.ch">billetterie@theatredupassage.ch</a>
+				</h4>
 
-					<h3 class="styled_text">Neuchâtel</h3>
-					<h4 class=""><span class="styled_text">1er, 3, 8 et 10 septembre 2017, Théâtre du Passage, Le Strapontin : </span>
-						<br>
-						<a href="qrcode.php" id="+41 32 717 79 07">+41 32 717 79 07</a>
-						<br>
-						<a href="mailto:billetterie@theatredupassage.ch" id="billetterie@theatredupassage.ch">billetterie@theatredupassage.ch</a></h4>
+				<br>
 
-						<br>
+				<h3 class="styled_text">Couvet</h3>
+				<h4 class="styled_text">18 et 19 novembre 2017, Chapelle aux Concerts, pas de réservation</h4>
 
-						<h3 class="styled_text">Couvet</h3>
-						<h4 class="styled_text">18 et 19 novembre 2017, Chapelle aux Concerts, pas de réservation</h4>
+				<br>
 
-						<br>
+				<h2><span class="styled_text">Distribution</span></h2>
+				<h3><span class="styled_text">La Vieille : </span><strong>Alice Grandjean</strong></h3>
+				<h3><span class="styled_text">Le Vieux : </span><strong>Léonard Schneider</strong></h3>
+				<h3><span class="styled_text">L'orateur : </span><strong>Vincent Steiner</strong></h3>
 
-						<h2><span class="styled_text">Distribution</span></h2>
-						<h3><span class="styled_text">La Vieille : </span><strong>Alice Grandjean</strong></h3>
-						<h3><span class="styled_text">Le Vieux : </span><strong>Léonard Schneider</strong></h3>
-						<h3><span class="styled_text">L'orateur : </span><strong>Vincent Steiner</strong></h3>
+				<br>
 
-						<br>
+				<h3><span class="styled_text">Piano : </span><strong>Carolyn Woods</strong></h3>
+				<h3><span class="styled_text">Accordéon : </span><strong>Cédric Liardet</strong></h3>
 
-						<h3><span class="styled_text">Piano : </span><strong>Carolyn Woods</strong></h3>
-						<h3><span class="styled_text">Accordéon : </span><strong>Cédric Liardet</strong></h3>
+				<br>
 
-						<br>
+				<h3><span class="styled_text">Mise en scène : </span><strong>Mathieu Alexandre</strong></h3>
+				<h3><span class="styled_text">Direction artistique et composition : </span><strong>Steve Muriset</strong></h3>
+				<h3><span class="styled_text">Décors : </span><strong>Frédéric Erb</strong></h3>
+				<h3><span class="styled_text">Costumes : </span><strong>Lucienne Amort-Muriset</strong></h3>
+				<h3><span class="styled_text">Eclairage, Maquillage : </span><strong>Faustine De Montmollin Brenier</strong></h3>
+				<h3><span class="styled_text">Chargé de Projet : </span><strong>Hugo Beretta</strong></h3>
 
-						<h3><span class="styled_text">Mise en scène : </span><strong>Mathieu Alexandre</strong></h3>
-						<h3><span class="styled_text">Direction artistique et composition : </span><strong>Steve Muriset</strong></h3>
-						<h3><span class="styled_text">Décors : </span><strong>Frédéric Erb</strong></h3>
-						<h3><span class="styled_text">Costumes : </span><strong>Lucienne Amort-Muriset</strong></h3>
-						<h3><span class="styled_text">Eclairage, Maquillage : </span><strong>Faustine De Montmollin Brenier</strong></h3>
-						<h3><span class="styled_text">Chargé de Projet : </span><strong>Hugo Beretta</strong></h3>
+				<br>
 
-						<br>
-
-					</div>
-
-					<div class="col-sm-4 col-lg-4">
-						<img class="img-responsive" src="images/flyer.png">
-					</div>
-
-				</div>
 			</div>
-			<?php include "includes/footer.php" ?>
-		</body>
-		</html>
+
+			<div class="col-sm-4 col-lg-4">
+				<img class="img-responsive" src="images/flyer.png">
+			</div>
+
+		</div>
+	</div>
+	<?php include "includes/footer.php" ?>
+</body>
+</html>
